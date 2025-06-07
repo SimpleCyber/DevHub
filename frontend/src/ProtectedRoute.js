5f95ce138a2f4d4d08b187a1d04bd370fedeb515
Satyam Yadav <satyamok03@gmail.com>
2025-06-07 12:50:14 +0530

    # Updated Loader

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -6,7 +6,9 @@ const ProtectedRoute = ({ children }) => {
   const { user, isLoading } = useUser();
 
   if (isLoading) {
-    return <div>Loading...</div>; 
+    return <div><div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div></div>; 
   }
 
   if (!user) {
